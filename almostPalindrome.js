7234c47291a50f07480abee969e7f1d5c260478d
Tarek <tareksherif23@yahoo.com>
2021-02-17 14:45:21 +0200

    #update

---
+++ almostPalindrome.js
@@ -0,0 +1,23 @@
+var validPalindrome = function(s) {
+    let start = 0;
+    let end = s.length - 1;
+    while (start < end) {
+        if (s[start] !== s[end]) {
+            return validSubPalindrome(s, start + 1, end) || validSubPalindrome(s, start, end - 1);
+        }
+        start++;
+        end--;
+    }
+    return true;
+  };
+  
+  var validSubPalindrome = function(s, start, end) {
+    while (start < end) {
+        if (s[start] !== s[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+  };
